2c77424bb1384837e9dfff6a51c8c9288ae526cf
vinniseryago <89748900+vinniseryago@users.noreply.github.com>
2024-10-22 18:46:03 +0300

    Add files via upload

--- ConsoleApp4/ConsoleApp1/Program.cs
+++ ConsoleApp4/ConsoleApp1/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Введите строку: ");
+        string input = Console.ReadLine();
+
+        Dictionary<char, int> letterCount = CountLetters(input);
+
+        Console.WriteLine("Количество букв:");
+        foreach (var kvp in letterCount)
+        {
+            Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+        }
+    }
+
+    public static Dictionary<char, int> CountLetters(string str)
+    {
+        Dictionary<char, int> countDict = new Dictionary<char, int>();
+
+        foreach (char c in str)
+        {
+            if (countDict.ContainsKey(c))
+            {
+                countDict[c]++;
+            }
+            else
+            {
+                countDict[c] = 1;
+            }
+        }
+
+        return countDict;
+    }
+}
--- ConsoleApp4/ConsoleApp2/Program.cs
+++ ConsoleApp4/ConsoleApp2/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите количество элементов массива:");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] numbers = new int[n];
+        Console.WriteLine("Введите элементы массива:");
+
+        for (int i = 0; i < n; i++)
+        {
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        Dictionary<int, int> numberCount = CountNumberOccurrences(numbers);
+
+        Console.WriteLine("Числа, встречающиеся в массиве нечетное количество раз:");
+        foreach (var kvp in numberCount)
+        {
+            if (kvp.Value % 2 != 0)
+            {
+                Console.WriteLine(kvp.Key);
+            }
+        }
+    }
+
+    public static Dictionary<int, int> CountNumberOccurrences(int[] arr)
+    {
+        Dictionary<int, int> countDict = new Dictionary<int, int>();
+
+        foreach (int num in arr)
+        {
+            if (countDict.ContainsKey(num))
+            {
+                countDict[num]++;
+            }
+            else
+            {
+                countDict[num] = 1;
+            }
+        }
+
+        return countDict;
+    }
+}
